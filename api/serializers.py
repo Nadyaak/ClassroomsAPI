b48137c36f9b9da98af1e01b777d34c27342adfe
Nadya <nadeia.ak@gmail.com>
2019-02-20 11:27:16 +0300

    Nadya-Atallah

--- api/serializers.py
+++ api/serializers.py
@@ -19,4 +19,4 @@ class ClassroomDetailSerializer(serializers.ModelSerializer):
 class ClassroomCreateUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Classroom
-        fields =' __all__'
+        fields ='__all__'
